0f6a6933ee3894a9e74a865e94007f436368e774
hhgz9527 <gaoyu@GaoYudeMacBook-Pro.local>
2014-04-16 23:00:59 +0800

    修改GData为cocoaPods

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios,'7.0'
+pod 'GData',"~>1.12.0"
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// GData
+#define COCOAPODS_POD_AVAILABLE_GData
+#define COCOAPODS_VERSION_MAJOR_GData 1
+#define COCOAPODS_VERSION_MINOR_GData 12
+#define COCOAPODS_VERSION_PATCH_GData 0
+
+// GData/Core
+#define COCOAPODS_POD_AVAILABLE_GData_Core
+#define COCOAPODS_VERSION_MAJOR_GData_Core 1
+#define COCOAPODS_VERSION_MINOR_GData_Core 12
+#define COCOAPODS_VERSION_PATCH_GData_Core 0
+
+// GData/XMLNode
+#define COCOAPODS_POD_AVAILABLE_GData_XMLNode
+#define COCOAPODS_VERSION_MAJOR_GData_XMLNode 1
+#define COCOAPODS_VERSION_MINOR_GData_XMLNode 12
+#define COCOAPODS_VERSION_PATCH_GData_XMLNode 0
+
+// GTMHTTPFetcher
+#define COCOAPODS_POD_AVAILABLE_GTMHTTPFetcher
+#define COCOAPODS_VERSION_MAJOR_GTMHTTPFetcher 0
+#define COCOAPODS_VERSION_MINOR_GTMHTTPFetcher 1
+#define COCOAPODS_VERSION_PATCH_GTMHTTPFetcher 0
+
+// gtm-oauth2
+#define COCOAPODS_POD_AVAILABLE_gtm_oauth2
+#define COCOAPODS_VERSION_MAJOR_gtm_oauth2 0
+#define COCOAPODS_VERSION_MINOR_gtm_oauth2 1
+#define COCOAPODS_VERSION_PATCH_gtm_oauth2 0
+
